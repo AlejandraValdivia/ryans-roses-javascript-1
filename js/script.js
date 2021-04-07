4b018e2141225900e6f8e5f180726d9793ad8b76
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2021-04-07 11:27:53 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var footer = document.querySelector("footer");
+console.log(footer);
+footer.style.backgroundColor = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+console.log(tagline);
+tagline.innerText = "Let us help you say it";
+
+var address = document.querySelector("address");
+address.lineHeight = "1.5em";
+address.fontSize = "1.75em";
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We are here for you <em>every day</em> of the week";
